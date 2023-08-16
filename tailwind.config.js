ad2b34a8ac0aa27e16632cd1b525b3dfc719fa55
Priyankar Lappy <priyankarkoley@gmail.com>
2023-08-16 23:46:23 +0530

    animation updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,34 +30,27 @@ module.exports = {
       },
       keyframes: {
         circlesm: {
-          "0%": { transform: "rotate(0deg) translateY(20px) rotate(0deg)" },
-          "100%": {
-            transform: "rotate(-360deg) translateY(20px) rotate(360deg)",
-          },
+          "0%": { transform: "rotate(0deg) translateY(20px) rotate(0deg) scale(0.8)"},
+          "50%": {transform: "rotate(-180deg) translateY(20px) rotate(180deg) scale(1.2)"},
+          "100%": {transform: "rotate(-360deg) translateY(20px) rotate(360deg) scale(0.8)"}
         },
         circlemd: {
-          "0%": { transform: "rotate(0deg) translateY(40px) rotate(0deg)" },
-          "100%": {
-            transform: "rotate(360deg) translateY(40px) rotate(-360deg)",
-          },
+          "0%": { transform: "rotate(0deg) translateY(40px) rotate(0deg) scale(0.8)"},
+          "50%": {transform: "rotate(318deg) translateY(40px) rotate(-180deg) scale(1.2)"},
+          "100%": {transform: "rotate(360deg) translateY(40px) rotate(-360deg) scale(0.8)"}
         },
         circlelg: {
-          "0%": { transform: "rotate(0deg) translateY(70px) rotate(0deg)" },
-          "100%": {
-            transform: "rotate(360deg) translateY(70px) rotate(-360deg)",
-          },
+          "0%": { transform: "rotate(0deg) translateY(70px) rotate(0deg) scale(0.8)"},
+          "50%": {transform: "rotate(318deg) translateY(70px) rotate(-180deg) scale(1.2)"},
+          "100%": {transform: "rotate(360deg) translateY(70px) rotate(-360deg) scale(0.8)"}
         },
         circlexl: {
-          "0%": { transform: "rotate(0deg) translateY(100px) rotate(0deg)" },
-          "100%": {
-            transform: "rotate(-360deg) translateY(100px) rotate(360deg)",
-          },
+          "0%": { transform: "rotate(0deg) translateY(100px) rotate(0deg) scale(0.8)"},
+          "50%": {transform: "rotate(-180deg) translateY(100px) rotate(180deg) scale(1.5)"},
+          "100%": {transform: "rotate(-360deg) translateY(100px) rotate(360deg) scale(0.8)"}
         },
       },
       animation: {
-        pulse1: "pulse 2s linear infinite",
-        pulse2: "pulse 4s linear infinite",
-        pulse3: "pulse 5s linear infinite",
         circlesm: "circlesm 12s linear infinite",
         circlemd: "circlemd 13s linear infinite",
         circlelg: "circlelg 15s linear infinite",
