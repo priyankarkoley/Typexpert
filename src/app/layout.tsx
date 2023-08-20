de725f90a9ff952e09315d4679cd7254e29f46be
Priyankar Lappy <priyankarkoley@gmail.com>
2023-08-20 13:26:38 +0530

    PWA Ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,11 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
         />
+        <link
+          rel="manifest"
+          href="/manifest.json"
+          // ../..//public
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
